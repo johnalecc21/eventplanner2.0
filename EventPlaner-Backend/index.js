d8eae07fb013525bc35fb86472efc56a4f6ed123
johnalecc21 <116313168+johnalecc21@users.noreply.github.com>
2025-02-14 15:18:51 -0500

    Update index.js

--- EventPlaner-Backend/index.js
+++ EventPlaner-Backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://eventplanner2-0.onrender.com/', 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
