00acc10b677f012df6efede8fe2e7be525f98091
john alejandro <johnalejandro316@gmail.com>
2025-02-14 15:13:36 -0500

    add just prod url to cors white list

--- EventPlaner-Backend/index.js
+++ EventPlaner-Backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://eventplanner2-0.onrender.com/'], 
+  origin: 'https://eventplanner2-0.onrender.com/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
